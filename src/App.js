fe7dd8a302fc4036ea5725df0d1d1c7736bd08a5
Remy Jacquet <jacquetr@iut1i-702-a16.local>
2019-11-20 17:49:42 +0100

    filtre tp2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CheckboxLabels from './components/CheckboxLabels';
+import BoutonMaPlanete from './components/BoutonMaPlanete';
+import EspeceListe from './components/EspeceListe';
+import AgeSlider from './components/AgeSlider';
 
 function App() {
-  return (
-	<CheckboxLabels />
+  	return (
+	<div className="content">
+
+		<div className="filters">
+			<h1 className="titreF">Filtres</h1>
+
+			<div className="maPlanete">
+				<BoutonMaPlanete />
+			</div>
+
+			<div className="sexe">
+				<CheckboxLabels />
+			</div>
+
+			<div className="age">
+				<AgeSlider />
+			</div>
+
+			<div className="espece">
+				<EspeceListe />
+			</div>
+
+		</div>
+
+		<div className="profiles">
+		</div>
+
+	</div>
   );
 }
 
