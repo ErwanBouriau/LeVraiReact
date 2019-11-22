6650dd849d66f9b63fe4c0595d57cf6c5642bc26
Erwan Bouriau <bouriaue@iut1i-702-a15.local>
2019-11-22 16:36:05 +0100

    profiles display

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -2,10 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import RoomIcon from '@material-ui/icons/Room';
 
 export default class RecipeReviewCard extends React.Component {
@@ -25,12 +22,12 @@ render() {
     const {name, first_name, localisation} = this.props;
     return (
         <Card style={styles.card}>
-        <CardMedia style={styles.photo} image="../profil-male.png"/>
+        <CardMedia style={styles.photo} image={require ("../profil-male.png")}/>
         <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
+            <Typography variant="h4" color="textSecondary" component="p">
                 {first_name} {name || ""} 
             </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
+            <Typography variant="body1" color="textSecondary" component="p">
             <RoomIcon /> {localisation}
             </Typography>
         </CardContent>
